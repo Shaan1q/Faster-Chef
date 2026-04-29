12e12250677cad70485ae55cb6d390efdd0da08d
competitioner <anon>
2026-04-29 10:30:34 -0400

    Initial Commit

---
+++ FasterChef/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FasterChef
+//
+//  Created by Shaanzay Iqbal on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FasterChef/FasterChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  FasterChefApp.swift
+//  FasterChef
+//
+//  Created by Shaanzay Iqbal on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct FasterChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
