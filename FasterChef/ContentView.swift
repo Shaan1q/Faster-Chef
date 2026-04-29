5fd4bb72c45ce8969433792806d6285c8be2a250
BT_C09_24 <mobileappdev@bths.edu>
2026-04-29 13:44:12 -0400

    Added the Views()

--- FasterChef/ContentView.swift
+++ FasterChef/ContentView.swift
@@ -11,13 +11,13 @@ struct ContentView: View {
     var body: some View {
         TabView{
             Tab ("Search", systemImage: "house"){
-                 
+                 SearchView()
                  }
             Tab ("Favorites", systemImage: "star"){
-                
+                FavoritesView()
             }
             Tab ("Recommendations", systemImage: "cloud.moon"){
-                
+                RecommendationView()
             }
         }
     
--- FasterChef/RecommendationView.swift
+++ FasterChef/RecommendationView.swift
@@ -5,3 +5,14 @@
 //  Created by Student on 4/29/26.
 //
 
+import SwiftUI
+
+struct RecommendationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationView()
+}
