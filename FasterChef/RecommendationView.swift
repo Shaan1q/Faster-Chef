4b1a4e6d834e72ff77374f2cba332795f61ee718
BT_09_09 <mobileappdev@bths.edu>
2026-04-29 13:54:07 -0400

    title

--- FasterChef/RecommendationView.swift
+++ FasterChef/RecommendationView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct RecommendationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Recommendations")
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+        }
     }
 }
 
