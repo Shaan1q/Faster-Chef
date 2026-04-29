96eb8534f5f47b53b2fe06014f33b97c5a6a7d1e
competitioner <anon>
2026-04-29 14:02:50 -0400

    Work on favourites page

--- FasterChef/FavoritesView.swift
+++ FasterChef/FavoritesView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct FavoritesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       VStack{
+           Rectangle()
+               .background(Color.yellow)
+               .frame(width: 500, height: 150)
+       }
+        
     }
 }
 
