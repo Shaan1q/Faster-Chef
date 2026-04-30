19a4119e938995df06bea949d11b71dcc6904dec
BT_C09_04 <mobileappdev@bths.edu>
2026-04-30 14:00:39 -0400

    Created DishCard

--- FasterChef/DishCard.swift
+++ FasterChef/DishCard.swift
@@ -12,13 +12,30 @@ struct DishCard: View {
     
     var body: some View{
         RoundedRectangle(cornerRadius: 40)
-            .foregroundStyle(.blue)
+            .foregroundStyle(Color("CardBgColor"))
             .frame(height: 200)
+            .shadow(color: .black, radius: 4, x: 2, y:3)
             .overlay{
                 HStack{
-                    
-                    Text(dish.strMeal)
-                    
+                    Spacer()
+                    VStack{
+                        Text("Name: \(dish.strMeal)")
+                        Text("Category: \(dish.strCategory)")
+                    }
+                    .foregroundStyle(.white)
+                    .bold()
+                    .font(.headline)
+                        let fixedImgURL = URL(string: dish.strMealThumb.replacingOccurrences(of: "\\", with: ""))
+                        AsyncImage(url: fixedImgURL) { receivedImage in
+                            receivedImage
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                              
+                        } placeholder: {
+                            ProgressView()
+                        }
+                    Spacer()
                 }
             }
             .padding([.leading, .trailing])
@@ -26,5 +43,5 @@ struct DishCard: View {
 }
 
 #Preview {
-    DishCard(dish: Dish(idMeal: 1235, strMeal: "test", strCategory: "test"))
+    DishCard(dish: Dish(idMeal: 1235, strMeal: "Spicy Arrabiata Penne", strCategory: "Vegetarian", strMealThumb: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"))
 }
