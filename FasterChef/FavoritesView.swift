41c2bcd0e20226f1df59ec4e803f60337981c1b9
competitioner <anon>
2026-04-30 14:01:30 -0400

    Changed Color

--- FasterChef/FavoritesView.swift
+++ FasterChef/FavoritesView.swift
@@ -10,15 +10,14 @@ import SwiftUI
 struct FavoritesView: View {
     var body: some View {
        VStack{
-
                ZStack{
                    Rectangle()
-                   .fill(Color.yellow)
+                   .fill(Color.niceYellow)
                     .frame(width: 500, height: 130, alignment: .top)
                    HStack(spacing: -200){
                        Circle()
                            .fill(Color.white)
-                           .stroke(Color.black, lineWidth: 5)
+                           .stroke(Color.black, lineWidth: 3)
                            .frame(width: 380, height: 160, alignment: .leading)
                            .overlay(
                        Image(systemName: "heart.circle")
