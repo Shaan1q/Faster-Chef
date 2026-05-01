bbeec52894e663a54ac022ffd5d2b3496ec68e9e
BT_09_09 <mobileappdev@bths.edu>
2026-05-01 14:02:33 -0400

    recs + background

--- FasterChef/RecommendationView.swift
+++ FasterChef/RecommendationView.swift
@@ -1,66 +1,76 @@
-//
-//  Recommendations.swift
-//  FasterChef
-//
-//  Created by Student on 4/29/26.
-//
-
 import SwiftUI
 
 struct RecommendationView: View {
     @State private var meal: Meal?
+
     var body: some View {
-        VStack{
+        VStack {
             Text ("Recommendations")
                 .font(.title)
                 .bold()
-        }
+        
             VStack(spacing: 20) {
                 if let meal = meal {
-                    Text(meal.mealName)
+                    Text(meal.strMeal)
                         .font(.title)
                         .bold()
-
-                    AsyncImage(url: URL(string: meal.mealImage)) { image in
+                    
+                    AsyncImage(url: URL(string: meal.strMealThumb)) { image in
                         image.resizable()
                             .scaledToFit()
                     } placeholder: {
                         ProgressView()
                     }
-                    ScrollView {
-                        Text(meal.strInstructions)
-                            .padding()
-                        }
+                    
+                   
+                } else {
+                    Text("Tap the button to for a recommendation! 🍽️")
                 }
-
-                Button("🍽️ Give me a recommendation! 🍽️") {
+                
+                Button("Get Random Meal") {
                     fetchMeal()
                 }
-                .foregroundStyle(.black)
-                .buttonStyle(.glass)
                 .padding()
             }
-            .padding()
+            
         }
+        .padding()
+        .applyBrandBackground()
+        
+    }
+
     func fetchMeal() {
-         
-      }
-  }
+        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/random.php") else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    let decoded = try JSONDecoder().decode(MealResponse.self, from: data)
+                    DispatchQueue.main.async {
+                        self.meal = decoded.meals.first
+                    }
+                } catch {
+                    print("Decoding error:", error)
+                }
+            }
+        }.resume()
+    }
+}
 
 
-    
+
 
 struct MealResponse: Codable {
     let meals: [Meal]
 }
 
 struct Meal: Codable {
-    let mealName: String
+    let strMeal: String
     let strInstructions: String
-    let mealImage: String
+    let strMealThumb: String
 }
-
 
 #Preview {
     RecommendationView()
 }
+
