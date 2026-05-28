f77c5c47ce010b49e69ad1d63936906402580472
BT_09_09 <mobileappdev@bths.edu>
2026-05-28 14:01:06 -0400

    added cutting board and knife

--- FasterChef/PrepView.swift
+++ FasterChef/PrepView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct PrepView: View {
+    @State private var animate = false
+    @State private var moveRight = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image ("CuttingBoard")
+                .resizable()
+                .scaledToFit()
+            VStack {
+                Text("PREP YOUR INGREDIENTS")
+                Text ("Click when you want to drop the knife and cut the ingredient")
+                
+            }
+            Image ("Knife")
+                .resizable()
+                .scaledToFit()
+                .offset(x: moveRight ? 100 : -100)
+                .onAppear {
+                    withAnimation(
+                        Animation.easeInOut(duration: 3.0)
+                            .repeatForever(autoreverses: false)
+                        value: moveRight
+                    )  .onAppear {
+                        moveRight.toggle()
+                    }
+                    
+                    
+                }
+        }
     }
 }
 
