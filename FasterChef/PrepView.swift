cfc02dbe2e58c745cead36a209f6163edd9cbb67
BT_09_09 <mobileappdev@bths.edu>
2026-05-29 14:02:18 -0400

    animated knife, added shallot

--- FasterChef/PrepView.swift
+++ FasterChef/PrepView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct PrepView: View {
     @State private var animate = false
     @State private var moveRight = false
-
+    @State private var isVisible = false
+    
     var body: some View {
         ZStack{
             Image ("CuttingBoard")
@@ -18,27 +19,42 @@ struct PrepView: View {
                 .scaledToFit()
             VStack {
                 Text("PREP YOUR INGREDIENTS")
+                    .font(.title)
                 Text ("Click when you want to drop the knife and cut the ingredient")
+                Text ("[TASK]")
+                Spacer()
                 
             }
+            Image("Shallot")
+                .resizable()
+                .scaledToFit()
+                        .opacity(isVisible ? 1 : 0) // Starts hidden
+                        .onAppear {
+                            withAnimation(.easeIn(duration: 1.0)) {
+                                isVisible = true // Triggers fade-in
+                            }
+                        }
             Image ("Knife")
                 .resizable()
                 .scaledToFit()
-                .offset(x: moveRight ? 100 : -100)
-                .onAppear {
-                    withAnimation(
-                        Animation.easeInOut(duration: 3.0)
-                            .repeatForever(autoreverses: false)
-                        value: moveRight
-                    )  .onAppear {
+                .offset(x: moveRight ? 150 : -150, y: 150)
+                .animation(
+                    Animation.linear(duration: 1.0)
+                        .repeatForever(autoreverses: true),
+                    value: moveRight
+                    )
+    
+                    .onAppear {
                         moveRight.toggle()
                     }
+         
+                    
                     
                     
                 }
         }
     }
-}
+
 
 #Preview {
     PrepView()
