31118e1e7495be59a6095603c2e27e20cafe72a9
BT_09_09 <mobileappdev@bths.edu>
2026-06-01 14:01:58 -0400

    knife stops

--- FasterChef/PrepView.swift
+++ FasterChef/PrepView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct PrepView: View {
-    @State private var animate = false
-    @State private var moveRight = false
-    @State private var isVisible = false
+    @State private var knifeX: CGFloat = -150
+    @State private var knifeY: CGFloat = 150
+    @State private var movingRight = true
+    @State private var timer: Timer?
+    @State private var shallotX: CGFloat = 0
+    @State private var showShallot = true
+
+       let shallotX: CGFloat = 0
     
     var body: some View {
         ZStack{
@@ -20,39 +25,80 @@ struct PrepView: View {
             VStack {
                 Text("PREP YOUR INGREDIENTS")
                     .font(.title)
+                    .bold()
+                    .fontDesign(.rounded)
+                    .padding (.top, 15)
+                    .padding (.bottom, 15)
                 Text ("Click when you want to drop the knife and cut the ingredient")
-                Text ("[TASK]")
+                    .padding (.bottom, 15)
+                    .fontDesign(.rounded)
+                Text ("Task: Cut Shallots")
+                Text ("Points: ")
+                    .padding (.top, 15)
+                    .fontDesign(.rounded)
                 Spacer()
+                    .fontDesign(.rounded)
                 
             }
             Image("Shallot")
                 .resizable()
                 .scaledToFit()
-                        .opacity(isVisible ? 1 : 0) // Starts hidden
-                        .onAppear {
-                            withAnimation(.easeIn(duration: 1.0)) {
-                                isVisible = true // Triggers fade-in
-                            }
-                        }
+            if showShallot {
+                Image("DicedShallot")
+                    .resizable()
+                    .scaledToFit()
+                    .offset(x: shallotX)
+            }
+            
             Image ("Knife")
                 .resizable()
                 .scaledToFit()
-                .offset(x: moveRight ? 150 : -150, y: 150)
-                .animation(
-                    Animation.linear(duration: 1.0)
-                        .repeatForever(autoreverses: true),
-                    value: moveRight
-                    )
-    
-                    .onAppear {
-                        moveRight.toggle()
+                .offset(x: knifeX, y: knifeY)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture{
+            stopKnife()
+        }
+        .onAppear {
+            startKnifeMovement()
+        }
+    }
+
+    func startKnifeMovement() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            
+            if movingRight {
+                knifeX += 2
+                
+                if knifeX >= 150 {
+                    movingRight = false
+                }
+            } else {
+                knifeX -= 2
+                
+                if knifeX <= -150 {
+                    movingRight = true
+                }
+            }
+        }
+    }
+    func stopKnife(){
+    let distance = abs(knifeX - shallotX)
+        if distance < 100 {
+            timer?.invalidate()
+            withAnimation(.easeIn(duration: 0.3)){
+                knifeY += 100
                     }
+                } else {
+                    print("Missed!")
+                }
+    }
+    
+    //add code to replace shallot with diced shallots
          
                     
                     
-                    
-                }
-        }
+        
     }
 
 
